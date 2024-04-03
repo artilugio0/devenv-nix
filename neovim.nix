15575138229be486de42b7838d222b72ce5499a9
artilugio0 <artilugio0>
2024-04-03 11:38:54 -0300

    change nvim config dir to not be a link to nix store

--- neovim.nix
+++ neovim.nix
@@ -11,8 +11,12 @@
     unzip
   ];
 
-  home.file.".config/nvim".source = builtins.fetchGit {
+  home.file.".config/nvim.original".source = builtins.fetchGit {
     url = "https://github.com/artilugio0/neovim-config";
     rev = "19afab164183a5e80d8f7e7ae9df6b57e26a4a48";
   };
+  home.file.".config/nvim.original".onChange = ''
+    $DRY_RUN_CMD rm -f $HOME/.config/nvim
+    $DRY_RUN_CMD cp -rp $HOME/.config/nvim.original/ $HOME/.config/nvim
+  '';
 }
