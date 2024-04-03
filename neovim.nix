ace0241558aad3a9f425c94e48ace30f12077e18
artilugio0 <artilugio0>
2024-04-03 00:06:13 -0300

    add neovim config from git repo

--- neovim.nix
+++ neovim.nix
@@ -10,4 +10,9 @@
     ripgrep
     unzip
   ];
+
+  home.file.".config/nvim".source = builtins.fetchGit {
+    url = "https://github.com/artilugio0/neovim-config";
+    rev = "19afab164183a5e80d8f7e7ae9df6b57e26a4a48";
+  };
 }
