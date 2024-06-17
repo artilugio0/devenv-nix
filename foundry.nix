3cf815dfe25d2f8f6a0660d2b9a7d3c3d5850947
artilugio0 <artilugio0>
2024-06-17 15:21:29 -0300

    add foundry files

--- foundry.nix
+++ foundry.nix
@@ -5,16 +5,16 @@
     unzip
   ];
 
-  home.file.".foundry".source = builtins.fetchZip {
-    url = "https://github.com/foundry-rs/foundry/releases/download/nightly-fd878884eda640c2cc59a7fa66aef9288846ef0e/foundry_nightly_linux_amd64.tar.gz";
-    hash = "";
+  home.file.".foundry/bin" = {
+    source = pkgs.fetchzip {
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-fd878884eda640c2cc59a7fa66aef9288846ef0e/foundry_nightly_linux_amd64.tar.gz";
+      hash = "sha256-59lJFQ0Ew1Y5jdPv4dtacm6WQJ7GSai0WcIsDXgz9v8=";
+      stripRoot = false;
+    };
+
+    onChange = ''
+    $DRY_RUN_CMD test -d $HOME/bin || mkdir $HOME/bin
+    $DRY_RUN_CMD find $HOME/.foundry/bin/ -exec ln -sf {} $HOME/bin/ \;
+    '';
   };
-  /*
-  home.file.".config/nvim.original".onChange = ''
-    $DRY_RUN_CMD test -d $HOME/.config/nvim && rm -fr $HOME/.config/nvim
-    $DRY_RUN_CMD cp -rp $HOME/.config/nvim.original/ $HOME/.config/nvim
-    $DRY_RUN_CMD find $HOME/.config/nvim -type d -exec chmod 755 {} \;
-    $DRY_RUN_CMD chmod 755 $HOME/.config/nvim
-  '';
-  */
 }
